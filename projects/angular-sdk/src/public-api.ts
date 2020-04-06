646c2b382ef82e4ef77557b17a9f781668544f2a
lmaksym <liepovskiy@gmail.com>
2020-04-06 13:15:11 +0300

    ci: 🎡 * build config

--- projects/angular-sdk/src/public-api.ts
+++ projects/angular-sdk/src/public-api.ts
@@ -1,7 +1,6 @@
 /*
  * Public API Surface of angular-sdk
  */
-
 export { FeatureToggleModule } from './modules/feature-toggle/feature-toggle.module';
 export { FeatureToggleService } from './modules/feature-toggle/services/feature-toggle.service';
 export { FEATURE_TOGGLE_CONFIG_TOKEN } from './modules/feature-toggle/feature-toggle-config.token';
